1f889b3cd46c3d1f794503b4ebdd1bf52d568dc2
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-02 16:23:29 -0700

    Adds myReduce function

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -20,3 +20,29 @@ Array.prototype.myMap = function(callback) {
   this.myEach(el => mappedArr.push(callback(el)) );
   return mappedArr;
 };
+
+Array.prototype.myReduce = function(callback, initialValue) {
+  let array = this;
+
+  if (!initialValue) {
+    initialValue = array[0];
+    array = array.slice(1);
+  }
+
+  let result = initialValue;
+  array.myEach(el => result = callback(result, el));
+  return result;
+
+};
+
+
+// this method doesnt work because this has to be reassigned
+// Array.prototype.myReduce = function(callback, initialValue) {
+// if (!initialValue) {
+//   initialValue = this[0];
+//   this.slice(1);
+// }
+//
+// this.myEach(el => result = callback(initialValue, el));
+// return initialValue;
+// };
